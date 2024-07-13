55c1c57166def7fdac4119d7eda81248e566a34d
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-07-13 22:28:18 +0330

    finish today learning

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 from flask import Flask
+
+import requests
+from flask import (
+    Flask,
+    Response,
+    abort,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
 app = Flask(__name__)
 
 
@@ -12,8 +26,12 @@ def main_page():
 def get_sms():
     pass
 
-def send_sms():
-    pass
+def send_sms(receptor, message):
+    """ gets a MSISDN and a message, then uses KaveNegar to send sms."""
+    url = f'https://api.kavenegar.com/v1/{config.API_KEY}/sms/send.json'
+    data = {"message": message,
+            "receptor": receptor}
+    res = requests.post(url, data) # type: ignore
 
 def check_serial():
     pass
